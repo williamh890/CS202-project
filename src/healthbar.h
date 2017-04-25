0807bdb1431dcaa0cb95f9b20cacf12381b5582e
williamh890 <wbhorn@alaska.edu>
2017-04-24 18:48:23 -0800

    Health bar is working and is based off of ships HP
    Uses 2 rectangle shapes to represent the players health. I think it would
    be nice if a similar concept was used to show the weapons reloading. Also
    health is now a float which does make things a lot nicer. There is still a
    lot of tweaking needed to be done in terms of game balance.

---
+++ src/healthbar.h
@@ -0,0 +1,28 @@
+//healthbar.h
+//Kyle Tam
+//4-20-17
+//Last rev 4-22-17
+
+#include "constants.h"
+
+#ifndef HEALTHBAR_H
+#define HEALTHBAR_H
+
+class HealthBar {
+
+public:
+    HealthBar(float maxHealth, float length = HP_BAR_LEN, float width = HP_BAR_WIDTH);
+    virtual  ~HealthBar();
+
+    void setCurrentHealthBar(float hpPercentage);
+
+    HPBarShape maxHealthBar;
+    HPBarShape currentHealthBar;
+
+private:
+    float _maxHealth;
+};
+
+#endif // HEALTHBAR_H
+
+
