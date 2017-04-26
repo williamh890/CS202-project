479f13ed3c91507ac0d2b8dd8b99cba259b4b8cf
Kvtam <kvtam@alaska.edu>
2017-04-26 15:55:37 -0800

    working on render function

--- src/gameover.cpp
+++ src/gameover.cpp
@@ -7,12 +7,13 @@
 #include "Ship.h"
 
 
+
 #include <SFML/Graphics.hpp>
 
 
 
 extern bool playerIsDead;
-bool drawGameOver;
+bool drawGameOver=true;
 
 sf::RectangleShape TextBox;
 
@@ -20,3 +21,23 @@ void initalDraw(){
 
 }
 
+
+int GameOverScreen::Run(sf::RenderWindow & gameMenu){
+    sf::Event event;
+    int menuSelect =7;
+    if(playerIsDead){//If player is dead draw the gameover text
+        shape.setFillColor(sf::Color::Red);
+        gameMenu.draw(TextBox);
+    }
+    if(drawGameOver)
+        initalDraw(gameMenu,drawGameOver);
+    while(true){
+        while(gameMenu.pollEvent(event)){
+            //check closed window
+            if(event.type==sf::Event::Closed)
+                return -1;
+
+        }
+    }
+
+}
